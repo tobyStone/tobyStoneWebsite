197e1feab62d5a6b2b59fe31af27d9d4dcca5d81
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-16 17:55:44 +0000

    ... landscape

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.6 (Hide Unmute)');
+    console.log('Initializing... Version: Contact Transition Update 1.7 (Mobile Landscape)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
