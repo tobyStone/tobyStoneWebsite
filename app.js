13dc13059e5159d981691878029e9a56206435ea
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-18 19:20:37 +0000

    V1.27 Portrait Refinements: Horizontal V2 text, moving Skip Intro

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.26 (V2 Portrait Refinements)');
+    console.log('Initializing... Version: Contact Transition Update 1.27 (V2 Horizontal Align)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
