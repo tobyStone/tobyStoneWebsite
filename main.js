353f8a10d15de0edfe3afc2e5ff6a6d8dae79b90
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-18 10:37:55 +0000

    portrait orientation css tweaks

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.19 (Skip Intro Position Check)');
+    console.log('Initializing... Version: Contact Transition Update 1.20 (Portrait Layout Tweaks)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
