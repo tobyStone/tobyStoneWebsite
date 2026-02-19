dbc40855ebb3c0b8138bd576fe0bed17bc321194
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-19 18:16:33 +0000

    ...

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.41 (Fine Tune Positions)');
+    console.log('Initializing... Version: Contact Transition Update 1.42 (V2 Enlarge + V3 Position)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
