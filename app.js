9228dffd9dbd21886c54257f44fa25edea2c6e66
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-19 16:20:21 +0000

    V1.38: Portrait Testimonials - Resize 3.77x, move to central image area

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.37 (CSS Order Fix)');
+    console.log('Initializing... Version: Contact Transition Update 1.38 (Testimonial Upgrade)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
