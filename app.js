53153c2c161ab42264450c80ab54adb0d818e2ec
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-19 15:26:39 +0000

    V1.34: Move Skip Intro CSS definition to allow overdrive, adjust Pow/Wow/Testimonials

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.33 (V3 Adjustments)');
+    console.log('Initializing... Version: Contact Transition Update 1.34 (Fix Skip Position & Scaling)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
