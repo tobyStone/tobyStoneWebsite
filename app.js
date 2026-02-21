d9926a29ef3b485e792390906846cec509a871d9
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2026-02-21 22:29:13 +0000

    update: contact email wording and form input opacity (v1.52)

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ const skipIntroBtn = document.getElementById('skip-intro-btn');
 let skipIntroTimeout;
 
 async function init() {
-    console.log('Initializing... Version: Contact Transition Update 1.50 (Refinements)');
+    console.log('Initializing... Version: Contact Transition Update 1.52 (Refinements)');
     // Setup Video 1
     video.src = videos.v1;
     video.muted = false; // Try sound first
